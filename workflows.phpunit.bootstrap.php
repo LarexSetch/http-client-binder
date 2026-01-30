f09688add65f0ca88ce65d871b04f0d85798cc5a
Roman Levchenko <larexsetch@gmail.com>
2026-01-30 13:57:29 +0300

    master

--- workflows.phpunit.bootstrap.php
+++ workflows.phpunit.bootstrap.php
@@ -2,15 +2,9 @@
 
 declare(strict_types=1);
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
 define('TMP_DIR', dirname(__FILE__) . '/var/tmp');
-define('WIREMOCK_HOST', 'http://localhost:8080');
+const WIREMOCK_HOST = 'http://localhost:8080';
 
 $loader = include __DIR__ . '/vendor/autoload.php';
-
-AnnotationRegistry::registerLoader(function ($className) use ($loader) {
-    $loader->loadClass($className);
-});
 
 return $loader;
